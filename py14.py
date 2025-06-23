24822c438c2b3bbe13d00184ba03e9f18bcded83
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2025-06-23 16:07:10 +0700

    update

--- py14.py
+++ py14.py
@@ -16,7 +16,7 @@ sys.stdout.reconfigure(encoding='utf-8')
 
 # Define file paths (equivalent to __DIR__ in PHP)
 current_dir = os.path.dirname(os.path.abspath(__file__))
-input_file = os.path.join(current_dir, 't24.pdf')
+input_file = os.path.join(current_dir, 't25.pdf')
 output_file = os.path.join(current_dir, 'cropped1169.png')
 
 array =[]
