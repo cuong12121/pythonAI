9d58746b75bd7757c9a8e90646eb76537727f7e9
cuong12121 <cuongdao12121@gmail.com>
2025-06-24 16:22:59 +0700

    Update py16.py

--- py16.py
+++ py16.py
@@ -143,12 +143,12 @@ def quantity(input_file):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     # Làm nét và threshold nhẹ (tùy ảnh)
-    # gray = cv2.bilateralFilter(gray, 11, 17, 17)
-    # _, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    gray = cv2.bilateralFilter(gray, 11, 17, 17)
+    _, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
     # OCR chỉ lấy số
     custom_config = r'--oem 3 --psm 6 outputbase digits'
-    result = pytesseract.image_to_string(image, config=custom_config, lang='eng')
+    result = pytesseract.image_to_string(image, config=custom_config)
     return result
 
 
