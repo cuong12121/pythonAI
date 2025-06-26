a41e42c89aa96fd5268292402159ef47c3258e2d
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2025-06-26 13:15:43 +0700

    Update py25py.py

--- py25py.py
+++ py25py.py
@@ -83,7 +83,7 @@ def sku(output_file):
     custom_config = r'--oem 3 --psm 11'
 
 
-    result = pytesseract.image_to_string(image, config=custom_config, lang='vie+eng')  # nếu có tiếng Việt
+    result = pytesseract.image_to_string(image, config=custom_config, lang='eng')  # nếu có tiếng Việt
     return result 
 
 
