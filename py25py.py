850682a9bba8fd6c0a6ad22806a91465d71770c0
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2025-06-26 11:56:06 +0700

    Update py25py.py

--- py25py.py
+++ py25py.py
@@ -81,7 +81,10 @@ def sku(output_file):
 
     # OCR chỉ lấy số
     custom_config = r'--oem 3 --psm 6'
-    result = pytesseract.image_to_string(image, config=custom_config, lang='eng')  # nếu có tiếng Việt
+
+    
+
+    result = pytesseract.image_to_string(image, config='--tessdata-dir /opt/tessdata', lang='eng')  # nếu có tiếng Việt
     return result 
 
 
