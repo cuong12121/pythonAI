3398d6f993d3f889d1050091a1c4b8703280f1ca
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2025-06-26 09:29:16 +0700

    Update py14.py

--- py14.py
+++ py14.py
@@ -91,7 +91,7 @@ skuss = re.sub(r"[^a-zA-Z0-9\- ]", "", skus)
 
 pattern = r'\b[A-Za-z0-9]{4}\s*-\s*[A-Za-z]{2}\s*-\s*\d{2}\s*-\s*[A-Za-z]{3}\b'
 
-clean_text = skuss.replace('\n', ' ').replace('\r', ' ')
+# clean_text = skuss.replace('\n', ' ').replace('\r', ' ')
 
 skusss = re.findall(pattern, clean_text)
 
