cbeb7d16b8cc23bb3dd62faf5a8f010aa59b5b88
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2025-06-26 07:55:30 +0700

    Update py14.py

--- py14.py
+++ py14.py
@@ -81,8 +81,8 @@ def sku(output_file):
     # _, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
     # OCR chỉ lấy số
-    custom_config = r'--oem 3 --psm 6'
-    result = pytesseract.image_to_string(image, config=custom_config, lang='vie')  # nếu có tiếng Việt
+    custom_config = r'--oem 3 --psm 11'
+    result = pytesseract.image_to_string(image, config=custom_config, lang='vie+eng')  # nếu có tiếng Việt
     return result 
 
 
