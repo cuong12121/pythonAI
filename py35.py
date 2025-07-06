68d57ed19b8e9e7d3a12c415a09a20ab4c6d9e8e
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2025-07-06 20:14:47 +0700

    update

--- py35.py
+++ py35.py
@@ -22,14 +22,14 @@ sys.stdout.reconfigure(encoding='utf-8')
 current_dir = os.path.dirname(os.path.abspath(__file__))
 # so_trang = count_pdf_pages(input_file)
 
-input_file = os.path.join(current_dir, 't43.pdf')
+input_file = os.path.join(current_dir, 't44.pdf')
 output_file = os.path.join(current_dir, 'cropped1169.png')
 
 # Đường dẫn đến file PDF
 pdf_path = input_file
 i=0
 # indexpage = i+1
-indexpage =34
+indexpage =2
 # Bước 1: Đọc chỉ trang 116 (số bắt đầu từ 1)
 pages = convert_from_path(pdf_path, dpi=500, first_page=indexpage, last_page=indexpage)
 
@@ -42,7 +42,7 @@ img = cv2.cvtColor(open_cv_image, cv2.COLOR_RGB2BGR)
 
 # Bước 4: Cắt vùng theo tọa độ [y1:y2, x1:x2]
 # Ví dụ: cắt vùng từ dòng 100 đến 400 và cột 200 đến 600
-cropped = img[3350:3900, 130:270]  # vì 82+88=170
+cropped = img[3350:4500, 130:270]  # vì 82+88=170
 
 cv2.imwrite("cropped_page119.png", cropped)
 
