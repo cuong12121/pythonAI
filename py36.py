44af317123e701b9d803f3ff0160de8c4093c86a
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2025-07-07 11:47:01 +0700

    Update py36.py

--- py36.py
+++ py36.py
@@ -64,7 +64,6 @@ def cut2(input_file):
 		elif preprocess == "blur":
 		    gray = cv2.medianBlur(gray, 5)
 
-
 		custom_config = r'--oem 3 --psm 6'
 		
 		from PIL import Image
@@ -73,18 +72,18 @@ def cut2(input_file):
 
 		skuss = re.sub(r'[^A-Za-z0-9]+', '-', text)
 
-    	skuss1 = skuss.replace('SKU', '')
+	    skuss1 = skuss.replace('SKU', '')
 
-    	skuss1 = skuss1.replace('SKU-','')      # Xóa 'SKU nếu có dấu gạch'
+	    skuss1 = skuss1.replace('SKU-','')      # Xóa 'SKU nếu có dấu gạch'
 
-    	pattern = r'\b[A-Za-z0-9]{4,5}\s*-\s*[A-Za-z]{2}\s*-\s*\d{2}\b'
+	    pattern = r'\b[A-Za-z0-9]{4,5}\s*-\s*[A-Za-z]{2}\s*-\s*\d{2}\b'
 
-    	clean_text = skuss1.replace('\n', ' ').replace('\r', ' ')
+	    clean_text = skuss1.replace('\n', ' ').replace('\r', ' ')
 
-    	skusss = re.findall(pattern, clean_text)
-    	skusss = [s.replace(" ", "") for s in skusss]
+	    skusss = re.findall(pattern, clean_text)
+	    skusss = [s.replace(" ", "") for s in skusss]
 
-    	rs = skusss
+	    rs = skusss
 	    
 	    array.append({
 	        'sku': rs
