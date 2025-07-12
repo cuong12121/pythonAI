0ab1916a24f62a1b976d27ce7a2717bc9d8cf4c9
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2025-07-12 11:45:12 +0700

    Update py42.py

--- py42.py
+++ py42.py
@@ -1,2 +1,12 @@
-import paddle
-print(paddle.__version__)
+from paddleocr import PaddleOCR
+
+# Khởi tạo PaddleOCR với ngôn ngữ tiếng Việt
+ocr = PaddleOCR(use_angle_cls=True, lang='vi')
+
+# Thay 'image.png' bằng đường dẫn tới hình ảnh của bạn
+image_path = 'cropped1170.png'
+result = ocr.ocr(image_path, cls=True)
+
+# In kết quả nhận diện
+for line in result[0]:
+    print(line[1][0])
