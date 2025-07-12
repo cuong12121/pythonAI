a47ad09385c2c13f78e9f88ec25b617bc1c6c156
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2025-07-12 11:47:14 +0700

    Update py42.py

--- py42.py
+++ py42.py
@@ -4,7 +4,7 @@ from paddleocr import PaddleOCR
 ocr = PaddleOCR(use_angle_cls=True, lang='en')
 
 # Thay 'image.png' bằng đường dẫn tới hình ảnh của bạn
-image_path = 'cropped1170.png'
+image_path = 'cropped1169.png'
 result = ocr.ocr(image_path, cls=True)
 
 # In kết quả nhận diện
