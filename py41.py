be74b096de5a48fda997bcc312beb054cd40cb39
cuong12121 <cuongdao12121@gmail.com>
2025-07-18 10:19:11 +0700

    update

--- py41.py
+++ py41.py
@@ -1,128 +1,85 @@
-
 import os
 import re
 import cv2
 import pytesseract
 import sys
 import io
-import time
-import numpy as np
-import gc
-from pdf2image import convert_from_path
-import fitz
-import requests
-import redis
+# import redis
 import json
-import time
+import requests
+import numpy as np
 from PIL import Image
 from wand.image import Image
 from wand.color import Color
+import pandas as pd
 
 sys.stdout.reconfigure(encoding='utf-8')
 
-current_dir = os.path.dirname(os.path.abspath(__file__))
+def count_pdf_pages(input_file):
+    with Image(filename=input_file) as img:
+        return len(img.sequence)
 
-import pdfplumber
+def cut_image(input_file,page):
+    # phần cắt ảnh thành 3 ảnh để check
+    with Image(filename=f'{input_file}[{page}]', resolution=(300, 300)) as img:
+        # Convert to grayscale
+        img.transform_colorspace('gray')
+        
+        for _ in range(1):
+            img.contrast()
+        
+        # Enhance image
+        img.modulate(brightness=500, saturation=500, hue=500)
+        
+        
+        with img.clone() as img3:    
+            img.transform_colorspace('gray')  # Convert to grayscale
 
-def flatten_text(cell):
-    return cell.replace('\n', '').strip() if cell else ''
+            for _ in range(1):
+                img.contrast()  # Increase contrast
 
+            img.modulate(brightness=500, saturation=500, hue=500)  # Enhance image
 
+            img.sharpen(radius=1, sigma=1)  # Sharpen image
 
-def extract_sku_and_quantity(pdf_path):
-    all_pages = []
-
-    with pdfplumber.open(pdf_path) as pdf:
-        total_pages = len(pdf.pages)
-
-        for i in range(total_pages):
-            page = pdf.pages[i]
-
-            text = page.extract_text()
-            tables = page.extract_tables()
-
-
-            # Tìm mã vận đơn
-            mvd_match = re.search(r'Mã vận đơn:\s*([A-Z0-9]+)', text)
-            # Tìm mã đơn hàng
-            order_code_match = re.search(r'Mã đơn hàng:\s*([A-Z0-9]+)', text)
-
-            mvd = mvd_match.group(1) if mvd_match else None
-            order_code = order_code_match.group(1) if order_code_match else None
-
-
-            page_items = []
-
-            for table in tables:
-                for row in table:
-                    if row and row[0] is not None and '#' in row[0]:
-                        header_index = table.index(row)
-                        product_rows = table[header_index + 1:]
-
-                        for r in product_rows:
-                            if r and r[0] and r[0].strip().isdigit():
-                                raw_sku = r[1]
-                                raw_qty = r[5]  # cột SL
-
-                                sku = raw_sku.replace('\n', '').strip() if raw_sku else ""
-                                try:
-                                    quantity = int(raw_qty.replace(',', '').strip()) if raw_qty else 0
-                                except:
-                                    quantity = 0
-
-                                page_items.append({
-                                    "mvd": mvd,
-                                    "mdh": order_code,
-                                    "sku": sku[:10],
-                                    "quantity": quantity,
-                                    "sku_full": sku,
-                                    "sku_full_check":sku[:10]
-
-                                })
-
-            all_pages.append(page_items)
-
-    return all_pages
-
-url = "https://drive.dienmayai.com/file_in.php"
-
-response = requests.get(url)
-
-data = response.json()  # Tự động decode JSON thành dict
-number = 0
-for item in data:
-    save_path = "t38.pdf"
-
-    file_pdf = 'https://dienmayai.com/'+item['file_pdf']
-
-    # Gửi request để tải file
-    responses = requests.get(file_pdf)
-
-    if responses.status_code == 200:
-
-        with open(save_path, 'wb') as f:
-            f.write(responses.content)
-            input_file = os.path.join(current_dir, 't38.pdf')
+            x = 82
+            y = 1900
+            width = 90
+            height = 1000
+            img.crop(x, y, width=width, height=height)
             
+            # Set output format to PNG
+            img.format = 'png'
+            
+           
 
-            array  = extract_sku_and_quantity(input_file)
+           
+            output_file ='cropped1170.png'
+            img.save(filename=output_file)        
 
-            # print(array)
+count =count_pdf_pages('t45.pdf')
+# for i in range(1):
+cut_image('t45.pdf',33)
+# image_path = 'cropped1170.png'  # hoặc đường dẫn đầy đủ nếu nằm ngoài thư mục chạy
 
-            r = redis.Redis(host='127.0.0.1', port=6379, decode_responses=True)
+# image = cv2.imread(image_path)
 
-
-            number += 1
-
-            key_name = 'orders:data_sku_tracking_code'+str(number)
-
-            # Nếu key tồn tại thì xóa
-
-
-            # Ghi dữ liệu mới
-            orders_json = json.dumps(array)
-            r.set(key_name, orders_json)    
-
+# text = pytesseract.image_to_string(image, config='--oem 3 --psm 6',lang='vie')
 
 
 
+# # Bước 1: Lấy phần sau đoạn "phân tiền\nloại"
+# match = re.search(r"phân tiền\s*loại\s*(.*)", text, re.DOTALL | re.IGNORECASE)
+
+# if match:
+#     after_text = match.group(1).strip()
+  
+#     print(after_text,flush=True)
+    #     # Bước 2: Tìm tất cả phần trước dấu '-' trong từng dòng
+    #     results = re.findall(r"^(.*?)-", after_text, re.MULTILINE)
+        
+    #     # In kết quả
+    #     for idx, item in enumerate(results, 1):
+    #         print(item.strip())
+    # else:
+    #     print("Không tìm thấy đoạn 'phan tién\\nloai'")
